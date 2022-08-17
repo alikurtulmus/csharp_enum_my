10d4bba610ac4cdc8caff2a79fbaa755f0cf044f
Ali Kurtulmus <mkurtulmus.eng@gmail.com>
2022-08-17 20:43:40 +0300

    Add project files.

---
+++ csharp_enum_my/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csharp_enum_my
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        enum sehirler { Sayı_Giriniz,Adana,Adıyaman,Afyon,Ağrı,Amasya,Ankara,Antalya,Artvin,Aydın}
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int plaka=Convert.ToInt16(textBox1.Text); //textbox'a girdiğimiz değeri int'e çevirip plaka değişkenine atadık
+            sehirler s; //sehirler isimli enum dizisinden s değişkeni türettik
+            s = (sehirler)plaka; //s değişkeninini sehirler dizisinin plaka değişkenine denk gelen elemanına eşitledik
+            //burada da dizilerdeki gibi elemanların indeksleri 0'dan başlıyor. o nedenle ilk elemana (0. indeksteki elemanı) şehir ismi vermedik
+            label1.Text = s.ToString();
+        }
+    }
+}
